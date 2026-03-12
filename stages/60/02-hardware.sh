012820d5f03d098702ecae927a211b7bc1fa9096
June Tate-Gans <june@theonelab.com>
2026-03-11 21:57:28 -0500

    Enable login prompt on the UART

--- stages/60/02-hardware.sh
+++ stages/60/02-hardware.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 echo 'dtoverlay=miniuart-bt' >>"$BOOTFS_PATH"/config.txt
+sed -e "s/#ttyS0/ttyAMA0/" -e "s/ttyS0/ttyAMA0/" -i "$ROOTFS_PATH"/etc/inittab
+echo ftdi_sio >"$ROOTFS_PATH"/etc/modules-load.d/ftdi.conf
 
